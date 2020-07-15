202cd2aad65756eb6cc9eace13bf7657f349257f
rflores-iterate <ray@iterate.ai>
2020-07-15 16:54:35 -0400

    add in price_type for saving values

--- wcpas.php
+++ wcpas.php
@@ -73,6 +73,7 @@
 		$addon_option_label       = $_POST['product_addon_option_label'];
 		$addon_option_price       = $_POST['product_addon_option_price'];
 		$addon_option_image       = $_POST['product_addon_option_image'];
+        $addon_option_price_type  = $_POST['product_addon_option_price_type'];
 		
 		
 		
