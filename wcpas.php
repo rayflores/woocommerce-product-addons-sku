a1d7f9a3d266f506154c1cdcb95ae769b0112494
rayflores <rayflores88@gmail.com>
2020-07-15 17:15:56 -0400

    bump version

--- wcpas.php
+++ wcpas.php
@@ -3,7 +3,7 @@
 	Plugin Name:  WooCommerce Product Addons Skus
 	Plugin URI: https://rayflores.com/plugins/wcpas/
 	Description: Add skus to product addons ( backend options )
-	Version: 0.4.5
+	Version: 0.4.6
 	Author: Ray Flores
 	Author URI: http://rayflores.com
 	*/
